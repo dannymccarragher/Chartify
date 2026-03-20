fdaca307627d5c1002dabdd864da48cc41620fa1
Danny McCarragher <dannymccarragher@gmail.com>
2026-03-20 13:26:48 -0700

    trust proxy for cors

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,8 @@ app.use(cors({
 app.use(express.json());
 const isProd = process.env.NODE_ENV === "production";
 
+app.set('trust proxy', 1);
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
